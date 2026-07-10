e58f70d24383f7430144f06035361de29fdd3fdf
vandanaramanjinappa <vandanabodapati22@gmail.com>
2026-07-10 19:51:27 +0530

    Added Flask code attribution comments - Source: Flask Documentation and YouTube tutorial

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # Code Attribution:
+# Flask framework reference:
+# https://python-adv-web-apps.readthedocs.io/en/latest/flask.html
+#
+# Flask tutorial reference:
+# https://youtu.be/oQ5UfJqW5Jo?si=c8DqVvnp8oKWG-2z
